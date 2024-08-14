b4abe3a05d176dfad51a32c7d4400507e663c14d
fum1h1to <75571344+fum1h1to@users.noreply.github.com>
2024-08-14 17:15:49 +0900

    add: metrics cached_principal_bytes and cached_principal_entries
    Signed-off-by: fum1h1to <75571344+fum1h1to@users.noreply.github.com>

--- service/metrics.go
+++ service/metrics.go
@@ -21,7 +21,12 @@ import (
 )
 
 const (
-	HTTP_ORIGIN_LATENCY = "http_origin_latency_in_seconds"
+	HTTP_ORIGIN_LATENCY             = "http_origin_latency_in_seconds"
+	HTTP_ORIGIN_LATENCY_HELP        = "Origin latency in seconds"
+	CACHED_PRINCIPAL_BYTES_METRIC   = "cached_principal_bytes"
+	CACHED_PRINCIPAL_BYTES_HELP     = "Number of bytes cached"
+	CACHED_PRINCIPAL_ENTRIES_METRIC = "cached_principal_entries"
+	CACHED_PRINCIPAL_ENTRIES_HELP   = "Number of entries cached"
 )
 
 type Metrics interface {
@@ -29,19 +34,47 @@ type Metrics interface {
 }
 
 type metrics struct {
-	httpOriginLatency prometheus.Histogram
+	httpOriginLatency      prometheus.Histogram
+	principalCacheSizeFunc func() int64
+	principalCacheLenFunc  func() int
 }
 
-func NewMetrics() (Metrics, error) {
+// MetricsOption is option for NewMetrics
+type MetricsOption func(*metrics)
+
+// WithPrincipalCacheSizeFunc set principal cache size function
+func WithPrincipalCacheSizeFunc(f func() int64) MetricsOption {
+	return func(m *metrics) {
+		m.principalCacheSizeFunc = f
+	}
+}
+
+// WithPrincipalCacheLenFunc set principal cache length function
+func WithPrincipalCacheLenFunc(f func() int) MetricsOption {
+	return func(m *metrics) {
+		m.principalCacheLenFunc = f
+	}
+}
+
+func NewMetrics(opts ...MetricsOption) (Metrics, error) {
 	m := &metrics{
 		httpOriginLatency: prometheus.NewHistogram(prometheus.HistogramOpts{
 			Name:    HTTP_ORIGIN_LATENCY,
-			Help:    "Origin latency in seconds",
+			Help:    HTTP_ORIGIN_LATENCY_HELP,
 			Buckets: prometheus.DefBuckets,
 		}),
 	}
 
+	for _, opt := range opts {
+		opt(m)
+	}
+
 	err := prometheus.Register(m.httpOriginLatency)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot register metrics")
+	}
+
+	err = prometheus.Register(m)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot register metrics")
 	}
@@ -57,4 +90,40 @@ func (m *metrics) Observe(name string, value float64) error {
 		return glg.Errorf("unknown metric name: %s", name)
 	}
 	return nil
+}
+
+// Collect is implementation of prometheus.Collector.Collect
+func (m *metrics) Collect(ch chan<- prometheus.Metric) {
+	// m.mutex.RLock()
+	// defer m.mutex.RUnlock()
+
+	// principal cache metrics
+	var metric prometheus.Metric
+	var err error
+	metric, err = prometheus.NewConstMetric(
+		prometheus.NewDesc(CACHED_PRINCIPAL_BYTES_METRIC, CACHED_PRINCIPAL_BYTES_HELP, nil, nil),
+		prometheus.GaugeValue,
+		float64(m.principalCacheSizeFunc()),
+	)
+	if err != nil {
+		glg.Errorf("Failed to create metric: %s", err.Error())
+	} else {
+		ch <- metric
+	}
+	metric, err = prometheus.NewConstMetric(
+		prometheus.NewDesc(CACHED_PRINCIPAL_ENTRIES_METRIC, CACHED_PRINCIPAL_ENTRIES_HELP, nil, nil),
+		prometheus.GaugeValue,
+		float64(m.principalCacheLenFunc()),
+	)
+	if err != nil {
+		glg.Errorf("Failed to create metric: %s", err.Error())
+	} else {
+		ch <- metric
+	}
+}
+
+// Describe is implementation of prometheus.Collector.Describe
+func (m *metrics) Describe(ch chan<- *prometheus.Desc) {
+	ch <- prometheus.NewDesc(CACHED_PRINCIPAL_BYTES_METRIC, CACHED_PRINCIPAL_BYTES_HELP, nil, nil)
+	ch <- prometheus.NewDesc(CACHED_PRINCIPAL_ENTRIES_METRIC, CACHED_PRINCIPAL_ENTRIES_HELP, nil, nil)
 }
